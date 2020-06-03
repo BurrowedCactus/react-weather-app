0f33c7860252ae04daf8fa5508dba4ef291110f9
Yongyuan Bai <y47bai@uwaterloo.ca>
2020-06-03 17:27:15 -0400

    Header - to be changed

---
+++ src/components/TrendGraph.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TrendGraph (props) {
+    return <div>
+        {props.children}
+        <h1>Hello world from trend graph</h1>
+    </div>;
+}
+
+
+export default TrendGraph;
